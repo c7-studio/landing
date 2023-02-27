acdf4a7701bf2f0e78041ff0773563b336eccee7
Flor <florencgmz@gmail.com>
2023-02-27 20:27:41 -0300

    fix animation

--- components/ServiceItem.jsx
+++ components/ServiceItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Card } from "flowbite-react";
 import { motion } from "framer-motion";
 
@@ -6,10 +6,10 @@ function ServiceItem({ itemKey, image, title, description }) {
 
   return (
     <motion.div 
-      initial={{opacity: 0, translateX: -50}}
+      initial={{ opacity: 0 }}
       viewport={{ once: true }}                  
       whileInView={{ opacity: 1, translateX: 0 }}
-      transition={{duration: 0.5, delay: itemKey * 0.8}}
+      transition={{ duration: 0.5, delay: itemKey * 0.8 }}
       className="max-w-sm sm:w-1/4 sm:mx-2 mt-2 sm:mt-0 mx-auto mb-20">
       <div>
         <Card
